f341a2d32b38ed59da3aee287ed6175fa488f449
Ruby <rchen139@my.bcit.ca>
2024-11-24 14:39:28 -0800

    Delete user page authenticate middleware as it is handled by the client side

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../database/users");
-const { authenticateToken } = require("./function/middleware");
-
-router.use(authenticateToken);
 
 /**
  * @swagger
