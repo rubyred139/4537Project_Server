7e054a40c87566322021543df836fa5f576a1644
Ruby <rchen139@my.bcit.ca>
2024-11-24 14:09:39 -0800

    Update frontend url

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: process.env.FRONTEND_URL,
 		credentials: true,
 	})
 );
